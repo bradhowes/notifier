bf879861b49d4a8e39f48208da2012b7b128e81b
Brad Howes <bradhowes@mac.com>
2012-04-15 19:16:02 +0000

    Changed tests to use vows.

--- payloadGeneratorTest.js
+++ payloadGeneratorTest.js
@@ -1,28 +1,34 @@
 var assert = require("assert");
-var PayloadGenerator = require("./payloadGenerator");
+var vows = require("vows");
+var PayloadGenerator =require("./payloadGenerator");
 var pg = new PayloadGenerator();
 
-module.exports = {
-
-    "test missing value" : function() {
-        var subs = { "BAR": "bar" };
-        var text = pg.transform("@@FOO@@ @@BAR@@", subs);
-        assert.equal(text, " bar");
+vows.describe('payloadGenerator').addBatch({
+    'handle missing values': {
+        topic: function() {
+            var subs = { "BAR": "bar" };
+            return pg.transform("@@FOO@@ @@BAR@@", subs);
+        },
+        'missing FOO': function(topic) {
+            assert.equal(topic, ' bar');
+        }
     },
-
-    "test default value" : function() {
-        var subs = { "BAR": "bar" };
-        var text = pg.transform("@@FOO=123@@ @@BAR@@", subs);
-        assert.equal(text, "123 bar");
+    'handle default values': {
+        topic: function() {
+            var subs = { "BAR": "bar" };
+            return pg.transform("@@FOO=123@@ @@BAR@@", subs);
+        },
+        'missing FOO': function(topic) {
+            assert.equal(topic, '123 bar');
+        }
     },
-
-    "test subs" : function() {
-        var subs = { "FOO": "foo", "BAR": "bar" };
-        var text = pg.transform("@@FOO@@ @@BAR@@", subs);
-        assert.equal(text, "foo bar");
+    'handle normal values': {
+        topic: function() {
+            var subs = { 'FOO': 'foo', 'BAR': 'bar' };
+            return pg.transform("@@FOO=123@@ @@BAR=456@@", subs);
+        },
+        'missing FOO': function(topic) {
+            assert.equal(topic, 'foo bar');
+        }
     }
-};
-
-if (typeof module !== "undefined" && module === require.main) {
-    require("asyncjs").test.testcase(module.exports).exec();
-}
+}).run();
