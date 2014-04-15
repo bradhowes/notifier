1de778c1caaec28198f9c6e95a3c38e100378bd8
Brad Howes <bradhowes@mac.com>
2014-04-15 12:41:19 +0200

    Added private info

---
+++ private/config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+
+    // Azure Table Store settings
+    'azure_storage_account': 'brhnotifier',
+    'azure_storage_access_key': 'fqSjMPX7HzwmYtCASy5n3KajFJcNyVghbjvvOQl9b/2JEmoLm2ubvplaMIPLZmMHmO2eD4SgVKg3h9KhZz0w9A==',
+
+    // Windows Notification Service (WNS)
+    'wns_client_secret': 'apqZLS7YyvZ3PdAWEnV0cR+PE0Wg1Kbs',
+    'wns_package_sid': 'ms-app://s-1-15-2-1999769064-1874154615-4172669911-680048040-2886621807-223654759-3790666136',
+
+    // Apple Push Notification Service (APNs)
+    'apns_root_certificate_file': 'EntrustRootCertificationAuthority.pem',
+    'apns_client_private_key_file': 'apn-nhtest-dev.pem',
+    'apns_client_certificate_file': 'apn-nhtest-dev.pem',
+
+    // Google Cloud Messaging (GCM)
+    'gcm_authorization_key': 'AIzaSyCsxY-KyFfHHn_PCkPByHEwtgjV7eeN0Eg'
+};
